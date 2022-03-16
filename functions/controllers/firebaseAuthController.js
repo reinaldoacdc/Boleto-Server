d327bac99899a605cec768c3d76690c6e809c41d
reinaldocosta <reinaldo.costa@gentrop.com>
2022-03-16 19:08:44 -0300

    feat: user poc

--- functions/controllers/firebaseAuthController.js
+++ functions/controllers/firebaseAuthController.js
@@ -1,25 +1,59 @@
-const admin = require("firebase-admin");
-//  acessing Cloud Firestore using the admin SDK locally
-let serviceAccount = require("../boleto-server-firebase-adminsdk.json");
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+const { initializeApp } = require('firebase-admin/app');
+const { getAuth, deleteUser, createUserWithEmailAndPassword } = require("firebase/auth")
 
-// creates a firestore instance
-const db = admin.firestore();
+const admin = require("firebase-admin");
+let serviceAccount = require("../boleto-server-firebase-adminsdk.json");
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+// });
+// const firebaseConfig = {
+//     credential: admin.credential.cert(serviceAccount)
+// };
+// const app = initializeApp(firebaseConfig);
+
+// initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+// });
 
 const create_user = (req, res) => {
-    let users = db.collection("boletos");
-    return users
-        .add(req.body)
-        .then(() => {
-            return res.status(201).json({ response: "user successfully created" });
+    const email = req.body.email
+    const password = req.body.password
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            return res.status(200).json({ response: user });
         })
-        .catch(error => {
+        .catch((error) => {
+            return res.status(500).json({ error: error });
+        });
+};
+
+const delete_user = (req, res) => {
+    admin.auth().deleteUser(req.body.id).then(() => {
+        return res.status(200).json({ response: 'ok' });
+    }).catch((error) => {
+        return res.status(500).json({ error: error });
+    });
+};
+
+
+const list_all = (req, res) => {
+    getAuth()
+        .listUsers(50)
+        .then((listUsersResult) => {
+            // listUsersResult.users.forEach((userRecord) => {
+            //     console.log('user', userRecord.toJSON());
+            // });
+            return res.status(200).json({ response: listUsersResult });
+        })
+        .catch((error) => {
             return res.status(500).json({ error: error });
         });
 };
 
 module.exports = {
-    create_user
+    create_user,
+    delete_user,
+    list_all
 };
--- functions/routes/routeuser.js
+++ functions/routes/routeuser.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const firebase = require("../controllers/firebaseAuthController");
+const controller = require("../controllers/firebaseAuthController");
 
-// router.get("/api/users", firestore.get_all_user);
+// router.get("/api/users", controller.list_all_users);
 // router.get("/api/user/:id", firestore.get_user);
 
 // router.put("/api/user/update/:id", firestore.update_user);
-// router.delete("/api/user/delete/:id", firestore.delete_user);
-router.post("/api/user/create", firestore.create_boleto);
+// router.delete("/api/user/delete/:id", controller.delete_user);
+// router.post("/api/user/create", controller.create_user);
 
 module.exports = router;
